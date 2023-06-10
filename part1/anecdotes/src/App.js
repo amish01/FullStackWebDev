e9f3fcc38856cb4d6ac6a2df6003c28173343b51
Aminu Abubakar <amish2g@gmail.com>
2023-06-10 11:15:20 +0000

    Complete exercises 1.6 - 1.14

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,6 +1,32 @@
 
 import { useState } from 'react'
 
+const getMax = (arr) => {
+  const iterator = arr.values()
+  let currMax = iterator.next().value
+  for (const votesCast of iterator){
+    if(votesCast > currMax){
+      currMax = votesCast
+    }
+  }
+  return currMax
+}
+
+const Display = ({anecdote, vote}) => {return (<div>
+                                      <h1>Anecdote of the day</h1>
+                                      <p>{anecdote} <br/> <span>This anecdote has <b>{vote}</b> votes</span></p>
+                                      </div>) }
+const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
+const Summary = ({anecdotesArr, votesArr}) => {
+  let maxVote = votesArr.indexOf(getMax(votesArr))  
+    return (
+    <div>
+      <h1>Anecdote with most votes</h1>
+      <p>{anecdotesArr[maxVote]}</p>  
+   </div>)
+  
+}
+
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often.',
@@ -14,10 +40,28 @@ const App = () => {
   ]
    
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(Array(8).fill(0, 0))
+  const numAnecdotes = anecdotes.length
+  
+  
 
+  const getNextAnecdote = () => {
+      const selectedIndex = Math.floor(Math.random() * numAnecdotes)
+      setSelected(selectedIndex)
+      return selectedIndex
+  } 
+  const voteSelectedAnecdote = () => {
+    const votesCopy = [...votes]
+    votesCopy[selected] += 1
+    setVotes(votesCopy)
+  }
+  
   return (
     <div>
-      {anecdotes[selected]}
+      <Display anecdote={anecdotes[selected]} vote={votes[selected]} />
+      <Button handleClick={voteSelectedAnecdote} text="vote" />
+      <Button handleClick={getNextAnecdote} text="next anecdote" />  
+      <Summary anecdotesArr={anecdotes} votesArr={votes} />       
     </div>
   )
 }
