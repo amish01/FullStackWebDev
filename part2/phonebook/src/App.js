59edde51bba8d957005e37d3046b41cd0c6ceff0
Aminu Abubakar <amish2g@gmail.com>
2023-06-24 21:50:26 +0000

    Complete exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 
 const PersonForm = ({handleAddName, 
@@ -37,16 +38,21 @@ const Filter = ({searchStr,searchArr, handleSearch}) => {
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchStr, setSearchStr] = useState('')
-  const [id, setId] = useState(persons.length + 1)
+  const [id, setId] = useState(persons.length)
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(() => response.data)
+      })
+  }, [])
+  console.log('render', persons.length, 'notes')
   let searchArr = []
  
 
@@ -69,14 +75,13 @@ const App = () => {
  
   const addName = (e) => {
     e.preventDefault()
-    //setId(persons.length + 1)
     let personObject
     if(newName){
-        setId(id + 1)
+        setId(persons.length + 1)
         personObject = {
         name: newName,
         number: newNumber? newNumber: `No number for ${newName}`,
-        id: id
+        id: id === 0 ? persons.length+1:id + 1
         }
     if(persons.map( n => n.name).indexOf(newName) < 0){
         setPersons(persons.concat(personObject))
